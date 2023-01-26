575fffe2368056a43d49ff4017fab276004acb3c
Luisa <luisa.mohle@no.experis.com>
2023-01-26 12:42:01 +0100

    Initial commit

---
+++ src/test/java/com/ansmeer/backendassignment3/BackendAssignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansmeer.backendassignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAssignment3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
