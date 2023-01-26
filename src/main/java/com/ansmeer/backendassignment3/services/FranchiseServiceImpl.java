f436dfe2aa87a892b6b17b531e22d8c9c0f5ed98
MHavre <marie.havre@no.experis.com>
2023-01-26 16:12:17 +0100

    Implement franchise service methods

--- src/main/java/com/ansmeer/backendassignment3/services/FranchiseServiceImpl.java
+++ src/main/java/com/ansmeer/backendassignment3/services/FranchiseServiceImpl.java
@@ -1,39 +1,52 @@
 package com.ansmeer.backendassignment3.services;
 
 import com.ansmeer.backendassignment3.models.Franchise;
+import com.ansmeer.backendassignment3.repositories.FranchiseRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
 @Service
 public class FranchiseServiceImpl implements FranchiseService {
+
+    private final FranchiseRepository repository;
+
+    public FranchiseServiceImpl(FranchiseRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
-    public Franchise findById(Integer integer) {
-        return null;
+    public Franchise findById(Integer id) {
+        return repository.findById(id).orElseThrow();
     }
 
     @Override
     public Collection<Franchise> findAll() {
-        return null;
+        return repository.findAll();
     }
 
     @Override
-    public Franchise add(Franchise entity) {
-        return null;
+    public Franchise add(Franchise franchise) {
+        return repository.save(franchise);
     }
 
     @Override
-    public Franchise update(Franchise entity) {
-        return null;
+    public Franchise update(Franchise franchise) {
+        return repository.save(franchise);
     }
 
     @Override
-    public int deleteById(Integer integer) {
+    public int deleteById(Integer id) {
+        //TODO: set franchise_id to null for movies
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return 1;
+        }
         return 0;
     }
 
     @Override
-    public int delete(Franchise entity) {
-        return 0;
+    public int delete(Franchise franchise) {
+        return deleteById(franchise.getId());
     }
 }
