abeab904164a2acf176da3b3a5a1915b9397799b
MHavre <marie.havre@no.experis.com>
2023-01-26 15:02:07 +0100

    Add repositories

---
+++ src/main/java/com/ansmeer/backendassignment3/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.ansmeer.backendassignment3.repositories;
+
+import com.ansmeer.backendassignment3.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
