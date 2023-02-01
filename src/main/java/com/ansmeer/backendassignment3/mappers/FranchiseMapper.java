048719f9ba702adf92c958ad815b4749ec587504
Luisa <luisa.mohle@no.experis.com>
2023-02-01 10:03:43 +0100

    Remove superfluous methods from franchise mapper

--- src/main/java/com/ansmeer/backendassignment3/mappers/FranchiseMapper.java
+++ src/main/java/com/ansmeer/backendassignment3/mappers/FranchiseMapper.java
@@ -1,35 +1,18 @@
 package com.ansmeer.backendassignment3.mappers;
 
 import com.ansmeer.backendassignment3.models.Franchise;
-import com.ansmeer.backendassignment3.models.Movie;
 import com.ansmeer.backendassignment3.models.dtos.franchise.FranchiseGetDTO;
-import com.ansmeer.backendassignment3.models.dtos.franchise.FranchiseGetMoviesDTO;
 import com.ansmeer.backendassignment3.models.dtos.franchise.FranchisePostDTO;
 import com.ansmeer.backendassignment3.models.dtos.franchise.FranchisePutDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface FranchiseMapper {
     FranchiseGetDTO franchiseToFranchiseGetDto(Franchise franchise);
 
     List<FranchiseGetDTO> franchiseToFranchiseGetDto(List<Franchise> franchise);
-
-    @Mapping(target = "movies", qualifiedByName = "moviesToMovieIds")
-    FranchiseGetMoviesDTO franchiseToFranchiseGetMoviesDto(Franchise franchise);
-
-    @Named(value = "moviesToMovieIds")
-    default Set<Integer> map(Set<Movie> value) {
-        if (value == null) {
-            return null;
-        }
-        return value.stream().map(Movie::getId).collect(Collectors.toSet());
-    }
 
     Franchise franchisePostDtoToFranchise(FranchisePostDTO franchise);
 
