c11ec916422a8cb5a6ac052eb33eae5ef25e47be
MHavre <marie.havre@no.experis.com>
2023-02-01 11:54:21 +0100

    Add swagger documentation to character controller

--- src/main/java/com/ansmeer/backendassignment3/controllers/CharacterController.java
+++ src/main/java/com/ansmeer/backendassignment3/controllers/CharacterController.java
@@ -6,6 +6,13 @@ import com.ansmeer.backendassignment3.models.dtos.character.CharacterGetDTO;
 import com.ansmeer.backendassignment3.models.dtos.character.CharacterPostDTO;
 import com.ansmeer.backendassignment3.models.dtos.character.CharacterPutDTO;
 import com.ansmeer.backendassignment3.services.character.CharacterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +33,18 @@ public class CharacterController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a character by their ID")
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Success",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = CharacterGetDTO.class))}
+    ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Character not found",
+                    content = @Content
+            )})
     public ResponseEntity<CharacterGetDTO> getById(@PathVariable int id) {
         return ResponseEntity.ok(
                 characterMapper.characterToCharacterDto(
@@ -33,6 +52,13 @@ public class CharacterController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all characters")
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Success",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = CharacterGetDTO.class)))}
+    )})
     public ResponseEntity<List<CharacterGetDTO>> getAll() {
         return ResponseEntity.ok(
                 characterMapper.characterToCharacterDto(
@@ -41,6 +67,12 @@ public class CharacterController {
     }
 
     @PostMapping
+    @Operation(summary = "Add a character")
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "201",
+            description = "Created",
+            content = @Content
+    )})
     public ResponseEntity<Object> add(@RequestBody CharacterPostDTO character) throws URISyntaxException {
         Character newCharacter = characterService.add(
                 characterMapper.characterPostDtoToCharacter(character));
@@ -49,6 +81,18 @@ public class CharacterController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a character")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Character updated",
+                    content = @Content),
+            @ApiResponse(responseCode = "404",
+                    description = "Character not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "400",
+                    description = "Bad request, URI does not match request body",
+                    content = @Content)
+    })
     public ResponseEntity<Object> update(@RequestBody CharacterPutDTO character, @PathVariable int id) {
         if (character.getId() != id) {
             return ResponseEntity.badRequest().build();
@@ -60,6 +104,17 @@ public class CharacterController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a character by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Character deleted",
+                    content = @Content),
+            @ApiResponse(responseCode = "404",
+                    description = "Character not found",
+                    content = @Content
+            )
+
+    })
     public ResponseEntity<Object> delete(@PathVariable int id) {
         characterService.deleteById(id);
         return ResponseEntity.noContent().build();
